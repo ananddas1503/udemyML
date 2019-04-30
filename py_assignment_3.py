04d4a340d4746d39a575f7adeba8f879bd359a7e
Anand Das <ananddas@Anands-MacBook-Air.local>
2019-04-30 11:11:14 +0530

    new commit on hand class

--- py_assignment_3.py
+++ py_assignment_3.py
@@ -46,26 +46,25 @@ class Card:
 # 3. define deck class that consists of different Card objects 
 
 class Deck:
-	def __init__(self):
-		self.deck = []
-		for suit in suits:
-			for rank in ranks:
-				self.deck.append(Card(suit,rank)) # build card objects and add them to the list seld.deck
-
-
-	def __str__(self):
-		deck_str=''
-		for x in self.deck:
-            deck_str += '\n '+x.__str__() # add each Card object's print string
-        return 'The deck has:' + deck_str
-
-
+    
+    def __init__(self):
+        self.deck = []  # start with an empty list
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit,rank))
+                
+    def __str__(self):
+        deck_comp = ''  # start with an empty string
+        for card in self.deck:
+            deck_comp += '\n '+card.__str__() # add each Card object's print string
+        return 'The deck has:' + deck_comp
+                
     def shuffle(self):
-    	random.shuffle(self.deck)
-
+        random.shuffle(self.deck)
+        
     def deal(self):
-    	single_card = self.deck.pop()
-    	return single_card
+        single_card = self.deck.pop()
+        return single_card
 
 
 
